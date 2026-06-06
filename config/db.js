226bb1d53e2e514ed8d018722d476f4c1805e426
parassecuritysystems-ops <parassecuritysystems@gmail.com>
2026-06-06 13:18:00 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,27 +1,30 @@
 const Datastore = require("nedb");
+const path = require("path");
+
+const DB_DIR = path.join(__dirname, "../database");
 
 const adminsDB = new Datastore({
-  filename: "./database/admins.db",
+  filename: path.join(DB_DIR, "admins.db"),
   autoload: true
 });
 
 const contactsDB = new Datastore({
-  filename: "./database/contacts.db",
+  filename: path.join(DB_DIR, "contacts.db"),
   autoload: true
 });
 
 const quotesDB = new Datastore({
-  filename: "./database/quotes.db",
+  filename: path.join(DB_DIR, "quotes.db"),
   autoload: true
 });
 
 const chatbotDB = new Datastore({
-  filename: "./database/chatbot.db",
+  filename: path.join(DB_DIR, "chatbot.db"),
   autoload: true
 });
 
 const otpDB = new Datastore({
-  filename: "./database/otp.db",
+  filename: path.join(DB_DIR, "otp.db"),
   autoload: true
 });
 
