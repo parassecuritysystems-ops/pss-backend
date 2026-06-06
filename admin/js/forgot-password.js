0de36f30db9020bf0db8cc20fec8b6dccea9b616
parassecuritysystems-ops <parassecuritysystems@gmail.com>
2026-06-06 12:29:44 +0530

    Update forgot-password.js

--- admin/js/forgot-password.js
+++ admin/js/forgot-password.js
@@ -4,7 +4,7 @@ async function sendOTP() {
 
   try {
 
-    const response = await fetch("http://localhost:5000/api/auth/forgot-password", {
+    const response = await fetch("https://api.parassecurity.in/api/auth/forgot-password", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
