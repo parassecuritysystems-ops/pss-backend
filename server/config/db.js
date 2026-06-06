bae9e92166bb802145410cc22fec25a2c5b6bd03
parassecuritysystems-ops <parassecuritysystems@gmail.com>
2026-06-06 11:13:12 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,27 +1,27 @@
 const Datastore = require("nedb");
 
 const adminsDB = new Datastore({
-  filename: "./server/database/admins.db",
+  filename: "./database/admins.db",
   autoload: true
 });
 
 const contactsDB = new Datastore({
-  filename: "./server/database/contacts.db",
+  filename: "./database/contacts.db",
   autoload: true
 });
 
 const quotesDB = new Datastore({
-  filename: "./server/database/quotes.db",
+  filename: "./database/quotes.db",
   autoload: true
 });
 
 const chatbotDB = new Datastore({
-  filename: "./server/database/chatbot.db",
+  filename: "./database/chatbot.db",
   autoload: true
 });
 
 const otpDB = new Datastore({
-  filename: "./server/database/otp.db",
+  filename: "./database/otp.db",
   autoload: true
 });
 
