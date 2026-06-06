03ffe304c6a7234f1f0d3b48d0e194dab354fb3b
parassecuritysystems-ops <parassecuritysystems@gmail.com>
2026-06-06 12:30:14 +0530

    Update quotes.js

--- admin/js/quotes.js
+++ admin/js/quotes.js
@@ -24,7 +24,7 @@ async function loadQuotes() {
   try {
 
     const response = await fetch(
-      "http://localhost:5000/api/admin/quotes",
+      "https://api.parassecurity.in/api/admin/quotes",
       {
         headers: {
           Authorization: `Bearer ${token}`
